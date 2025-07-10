0d259ac785145e1655930ad79e1bdc29d9e5db81
zer356v <lakshmisai55700@gmail.com>
2025-07-10 11:24:10 +0530

    Last

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { PackageProvider } from './utils/data.jsx';
+import { PackageProvider } from './utils/Data.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
- 
   <PackageProvider>
     <App />
   </PackageProvider>
